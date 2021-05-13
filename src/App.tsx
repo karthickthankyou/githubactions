63bcebd2a9784e57461a26959bae33296166c184
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-05-13 22:36:42 +0530

    testing build review

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 2
+          Learn React in an easy way!
         </a>
       </header>
     </div>
