3ece8d46df6bf28364c85ee3981b0defc3801bff
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-05-13 22:12:41 +0530

    next nice commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
@@ -16,11 +16,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 2
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
